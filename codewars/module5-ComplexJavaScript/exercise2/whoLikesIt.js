8caa9e050b6c956ef0f7f2c2a5d1e2a2c5347f4b
scott <scottysora@gmail.com>
2024-05-29 23:32:44 -0400

    CJS 2.3 & 2.4

--- codewars/module5-ComplexJavaScript/exercise2/whoLikesIt.js
+++ codewars/module5-ComplexJavaScript/exercise2/whoLikesIt.js
@@ -18,6 +18,23 @@ function likes(names) {
     case 3:
       return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     default:
-      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+      return `${names[0]}, ${names[1]} and ${
+        names.length - 2
+      } others like this`;
   }
 }
+
+//* solution 2
+// function likes(names) {
+//   if (names.length === 0) {
+//     return "no one likes this";
+//   } else if (names.length === 1) {
+//     return names[0] + " likes this";
+//   } else if (names.length === 2) {
+//     return names[0] + " and " + names[1] + " like this";
+//   } else if (names.length === 3) {
+//     return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+//   } else {
+//     return (names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this");
+//   }
+// }
