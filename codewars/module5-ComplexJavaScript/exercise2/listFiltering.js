f359dc191569418b3b9a3fb553c135ec25a5a1ad
scott <scottysora@gmail.com>
2024-05-31 00:53:33 -0400

    mob code answers & explanations

--- codewars/module5-ComplexJavaScript/exercise2/listFiltering.js
+++ codewars/module5-ComplexJavaScript/exercise2/listFiltering.js
@@ -1,4 +1,5 @@
-//* create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+//* create a function that takes a list of non-negative integers and strings 
+//* and returns a new list with the strings filtered out.
 
 function filter_list(l) {
   // Filter out strings from the list
